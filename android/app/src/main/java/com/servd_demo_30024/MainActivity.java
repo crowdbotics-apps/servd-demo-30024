45afc990f530fb6b603af75417dad187bf8f4239
Crowdbotics <team@crowdbotics.com>
2021-08-22 17:41:02 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/servd_demo_30024/MainActivity.java
@@ -0,0 +1,15 @@
+package com.servd_demo_30024;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "servd_demo_30024";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "servd_demo_30024Identifier",
+  url: "https://servd_demo_30024Identifier.botics.co",
+  api: "https://servd_demo_30024Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
